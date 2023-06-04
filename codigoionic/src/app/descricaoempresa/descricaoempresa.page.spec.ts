d463d7850e599f51cfc55cb0f310fdc55ed7995e
lohangr <contatogr@msn.com>
2023-06-04 02:47:43 -0300

    ajustando scss de algumas paginas e refazendo page descricaoempresa

--- codigoionic/src/app/descricaoempresa/descricaoempresa.page.spec.ts
+++ codigoionic/src/app/descricaoempresa/descricaoempresa.page.spec.ts
@@ -1,15 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DescricaoempresaPage } from './descricaoempresa.page';
+import { IonicModule } from '@ionic/angular';
 
 describe('DescricaoempresaPage', () => {
   let component: DescricaoempresaPage;
   let fixture: ComponentFixture<DescricaoempresaPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(DescricaoempresaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
